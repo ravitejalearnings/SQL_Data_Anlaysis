f8f32b3a3eb2da3fb60c746ae0b38a592ec33ed0
Ravi Teja Anumandla <ravianumandla69@gmail.com>
2025-06-27 16:08:40 +0530

    Create ddl.sql

---
+++ scripts/ddl.sql
@@ -0,0 +1,63 @@
+create database datawarehouse_analytics
+
+use datawarehouse_analytics
+
+==========================================================
+-- Creating ddl for customer, products, sales
+==========================================================
+
+-- ddl for dim_customer
+
+if object_id('dim_customer', 'U') is not null
+	drop table dim_customer;
+go
+create table dim_customer(
+	customer_key int,
+	customer_id int,
+	customer_number nvarchar(50),
+	first_name  nvarchar(50),
+	last_name  nvarchar(50),
+	country  nvarchar(50),
+	marital_status  nvarchar(10),
+	gender  nvarchar(10),
+	birth_date  date,
+	create_date  date
+);
+
+
+-- ddl for dim_products
+
+if object_id('dim_products', 'U') is not null
+	drop table dim_products;
+go
+create table dim_products(
+	product_key  int,
+	product_id  int,
+	product_number nvarchar(50),
+	product_name nvarchar(50),
+	category_id nvarchar(50),
+	category nvarchar(50),
+	subcategory nvarchar(50),
+	maintenance nvarchar(5),
+	cost int,
+	product_line nvarchar(50),
+	start_date date
+	
+);
+
+-- ddl for fact_sales
+
+if object_id('fact_sales', 'U') is not null
+	drop table fact_sales;
+go
+create table fact_sales(
+	order_number nvarchar(50),
+	product_key int,
+	customer_key int,
+	order_date date,
+	shipment_date date,
+	due_date date,
+	sales int,
+	quantity int,
+	price int
+);
